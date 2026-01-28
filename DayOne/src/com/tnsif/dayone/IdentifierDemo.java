ca661ef9678d1429ff4e5619298ef72c3930f802
Shankar-Sala <shankarsala9995@gmail.com>
2026-01-28 19:13:52 +0530

    Identifier

---
+++ DayOne/src/com/tnsif/dayone/IdentifierDemo.java
@@ -0,0 +1,22 @@
+package com.tnsif.dayone;
+
+public class IdentifierDemo {
+
+	public static void main(String[] args) {
+		//variable declaration
+		//int static=13; // error becoz keyword cannot be identifier
+//		int num1 = 13; error becoz do not add space
+//		int num = 13;  error becoz special symbols not allowed
+		
+		int $num1 = 20;
+		System.out.println("Value of the number is : " + $num1);
+		
+		String studentName="Shankar";
+		System.out.println("Student name is : "+ studentName);
+		
+		char gender = 'm';
+		System.out.println("Gender :"+gender);
+
+	}
+
+}
